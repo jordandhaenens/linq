7e3b26f487f7d7908fa51872e42a5abc356408ff
Jordan Dhaenens <jordan.dhaenens@gmail.com>
2017-07-11 21:43:30 -0500

    finished up exercises on aggregating

--- Program.cs
+++ Program.cs
@@ -66,7 +66,28 @@ namespace linq
             //     Console.WriteLine(number);
             // }
 
-            
+            // Output how many numbers are in this list
+            List<int> numbers1 = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+            // Console.WriteLine(numbers1.Count());
+
+            // How much money have we made?
+            List<double> purchases = new List<double>()
+            {
+                2340.29, 745.31, 21.76, 34.03, 4786.45, 879.45, 9442.85, 2454.63, 45.65
+            };
+            // Console.WriteLine("The sum of the purchases is {0}", purchases.Sum());
+
+            // What is our most expensive product?
+            List<double> prices = new List<double>()
+            {
+                879.45, 9442.85, 2454.63, 45.65, 2340.29, 34.03, 4786.45, 745.31, 21.76
+            };
+            // Console.WriteLine(prices.Max());
+
+
 
         }
     }
